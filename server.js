f2a9b01f989862b50fc989845974697ce6850352
Callie Nipper <callie.nipper@gmail.com>
2021-05-18 15:05:28 -0700

    Created server.js file

---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+
+const db = require("./models");
+
+const app = express();
+
+// Middleware
+app.use(logger("dev"));
+
+// Parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static Files
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+//Routes
+require('./controllers/api-routes')
+require('./controllers/html-routes')
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`)
+});
